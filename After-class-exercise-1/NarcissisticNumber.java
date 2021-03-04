547098b1096c9c01471370f700f7c6d1f83ee87f
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-03-04 16:27:11 +0800

    Update NarcissisticNumber.java

--- After-class-exercise-1/NarcissisticNumber.java
+++ After-class-exercise-1/NarcissisticNumber.java
@@ -1,3 +1,9 @@
+/* 1. 水仙花数（ narcissistic number ）是指一个 n 位数 ( n≥3 )，它的每
+个位上的数字的 n 次幂之和等于它本身。
+例如：1^3 + 5^3 + 3^3 = 153
+
+编写相应的Java程序，获取所有的三位水仙花数。
+ */
 public class NarcissisticNumber {
 
     public static void main(String[] args) {
