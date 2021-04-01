bfd4ad281e353b9c54355632b53ddda2f91361bf
周晨阳 <1448517833@qq.com>
2021-04-01 16:10:04 +0800

    Update Box.java

--- Experiment-2/Box.java
+++ Experiment-2/Box.java
@@ -1,9 +1,11 @@
 public class Box {
-    private static Box box = new Box();
+    private static Box box = new Box();// 创建Box的一个对象
 
+    // 让构造方法为private，这样该类就不会被实例化
     private Box() {
     }
 
+    // 获取唯一可用的对象
     public static Box getBox() {
         return box;
     }
