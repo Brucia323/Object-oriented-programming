00ce96d99bacec878312f49a213b4a5338d5120d
周晨阳 <1448517833@qq.com>
2021-04-01 16:10:07 +0800

    Update BoxFactory.java

--- Experiment-2/BoxFactory.java
+++ Experiment-2/BoxFactory.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class BoxFactory {
     public static void main(String[] args) {
         int length = 0, width = 0, height = 0;
-        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入盒子的规格：");
+        Scanner scanner = new Scanner(System.in);// 从键盘接收数据
+        // 判断是否还有输入
         if (scanner.hasNext()) {
             length = scanner.nextInt();
             width = scanner.nextInt();
@@ -11,7 +13,7 @@ public class BoxFactory {
         }
         scanner.close();
         Configuration configuration = new Configuration(length, width, height);
-        Box box = Box.getBox();
-        box.getVolume();
+        Box box = Box.getBox();// 获取唯一可用的对象
+        box.getVolume();// 显示体积
     }
 }
