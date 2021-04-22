f3c28fa66c250fc1f3f2ff2cf80266d69e467da0
周晨阳 <1448517833@qq.com>
2021-04-22 08:21:46 +0800

    Create Terricole.java

---
+++ Experiment-3/Terricole.java
@@ -0,0 +1,7 @@
+public abstract class Terricole implements Crawlable, Runable, Jumpable {
+    public abstract void crawl();// 爬行
+
+    public abstract void run();// 跑动
+
+    public abstract void jump();// 跳跃
+}
