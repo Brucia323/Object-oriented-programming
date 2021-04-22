19fbf74e05bb16ae76ceb81cde829041f0f31a55
周晨阳 <1448517833@qq.com>
2021-04-22 08:21:48 +0800

    Create Trousers.java

---
+++ Experiment-3/Trousers.java
@@ -0,0 +1,5 @@
+public class Trousers implements Producible{
+    public void getTrousers(String sexString, String colorString, String sizeString) {
+        System.out.println("这是一件" + colorString + "色的" + sizeString + "码" + sexString + "性裤子");
+    }
+}
