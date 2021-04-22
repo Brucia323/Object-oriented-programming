322babdda50afea908e24b8bba06ca8b3d005fb2
周晨阳 <1448517833@qq.com>
2021-04-22 08:21:30 +0800

    Create Fish.java

---
+++ Experiment-3/Fish.java
@@ -0,0 +1,35 @@
+public class Fish extends Aquatilia {
+    private String color;// 颜色
+    private String size;// 大小
+    private String info;// 其它
+
+    public void setColor(String colorString) {
+        color = colorString;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setSize(String sizeString) {
+        size = sizeString;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setInfo(String infoString) {
+        info = infoString;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    @Override
+    public void swim() {
+        // TODO Auto-generated method stub
+        System.out.println("游泳");
+    }
+}
