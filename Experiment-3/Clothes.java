ba402fc5738ad44fed14b42965603653fa0b861a
周晨阳 <1448517833@qq.com>
2021-04-22 18:13:57 +0800

    Update Clothes.java

--- Experiment-3/Clothes.java
+++ Experiment-3/Clothes.java
@@ -1,5 +1,3 @@
-public class Clothes implements Producible{
-    public void getClothes(String sexString, String colorString, String sizeString) {
-        System.out.println("这是一件" + colorString + "色的" + sizeString + "码" + sexString + "性衣服");
-    }
+public abstract class Clothes {
+    public abstract void show();//
 }
