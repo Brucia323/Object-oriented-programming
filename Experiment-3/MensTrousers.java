fc268bdede5c7e2d2166ded337968ddb09eb6207
周晨阳 <1448517833@qq.com>
2021-04-22 18:14:06 +0800

    Create MensTrousers.java

---
+++ Experiment-3/MensTrousers.java
@@ -0,0 +1,9 @@
+public class MensTrousers extends Trousers{
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+        System.out.println("生产一件男性裤子");
+    }
+
+}
