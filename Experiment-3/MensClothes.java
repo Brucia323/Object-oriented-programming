aa5785308df3fd01e8cb986a4fb4b08d0d5c842d
周晨阳 <1448517833@qq.com>
2021-04-22 18:14:00 +0800

    Create MensClothes.java

---
+++ Experiment-3/MensClothes.java
@@ -0,0 +1,9 @@
+public class MensClothes extends Clothes {
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+        System.out.println("生产一件男性衣服");
+    }
+
+}
