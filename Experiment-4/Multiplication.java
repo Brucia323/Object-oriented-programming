1330aa0697db24953663676ede4111b206bdcc9b
周晨阳 <1448517833@qq.com>
2021-04-29 14:50:54 +0800

    Update Multiplication.java

--- Experiment-4/Multiplication.java
+++ Experiment-4/Multiplication.java
@@ -1,13 +1,34 @@
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * 乘法表类
+ *
+ * @throws IOException
+ * @version 1.0
+ */
 public class Multiplication {
+    /** 结果 */
     private int result;
 
+    /**
+     * 设置结果
+     *
+     * @param resultInteger
+     * @since 1.0
+     * @version 1.0
+     */
     public void setResult(int resultInteger) {
         result = resultInteger;
     }
 
+    /**
+     * 写入文件
+     *
+     * @throws IOException
+     * @since 1.0
+     * @version 1.0
+     */
     public void writer() throws IOException {
         // FileWriter fw = new FileWriter("D:/data.txt");
         FileWriter fileWriter = new FileWriter("D:/data.txt");// 定义FileWriter类
