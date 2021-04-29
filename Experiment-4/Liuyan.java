102c65c9461f4006ac9e9507dae0ef330dd72f3d
周晨阳 <1448517833@qq.com>
2021-04-29 14:44:14 +0800

    Update Liuyan.java

--- Experiment-4/Liuyan.java
+++ Experiment-4/Liuyan.java
@@ -2,33 +2,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 留言板
+ *
+ * @param args
+ * @author 周晨阳
+ * @version 1.0
+ */
 public class Liuyan {
+    /**
+     * 主方法
+     *
+     * @param args
+     * @version 1.0
+     */
     public static void main(String[] args) {
         System.out.println("*****Welcome to use Message Board*****");
         System.out.println("Please Enter Your Order:");
         // InputStreamReader reader = new InputStreamReader(System.in);
         // BufferedReader reader2 = new BufferedReader(reader);
-        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String order;
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);// 使用System.in定义InputStreamReader类
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);// 使用inputStreamReader定义BufferedReader类
+        String order;// 定义指令
         try {
             while ((order = bufferedReader.readLine()) != null) {
                 if (order.equals("add")) {
                     System.out.println("Enter Message's author:");
-                    String author = bufferedReader.readLine();
+                    String author = bufferedReader.readLine();// 定义作者
                     System.out.println("Enter Message's title:");
-                    String title = bufferedReader.readLine();
+                    String title = bufferedReader.readLine();// 定义标题
                     System.out.println("Enter Message's content:");
-                    String content = bufferedReader.readLine();
-                    MessageSet messageSet = new MessageSet(author, title, content);
+                    String content = bufferedReader.readLine();// 定义内容
+                    MessageSet messageSet = new MessageSet(author, title, content);// 使用author,title,content定义MessageSet类
                     messageSet.messageSet();
                     System.out.println("留言成功！");
                     System.out.println("*****************************");
                 }
                 if (order.equals("display")) {
                     System.out.println("*****************************");
-                    int messageNumber = 1;
-                    Message message = new Message();
+                    int messageNumber = 1;// 定义留言编号，默认值为1
+                    Message message = new Message();// 定义Message类
                     while (message.message()) {
                         System.out.println("Message" + messageNumber + ":");
                         System.out.println("author:" + message.getAuthor());
