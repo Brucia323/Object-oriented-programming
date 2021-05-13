c38f5861ce1675eb8db7d82c074ba19383a20839
周晨阳 <1448517833@qq.com>
2021-05-13 16:20:57 +0800

    Create Untitled.java

---
+++ Experiment-5/Untitled.java
@@ -0,0 +1,69 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Untitled extends Applet implements ActionListener {
+    Label firstLabel, secondLabel, thirdLabel;
+    Button leftButton, centerButton, rightButton;
+    int x1, y1, x2, y2, x, y;
+    String str;
+
+    @Override
+    public void init() {
+        firstLabel = new Label("first");
+        secondLabel = new Label("second");
+        thirdLabel = new Label("third");
+        leftButton = new Button("left");
+        centerButton = new Button("center");
+        rightButton = new Button("right");
+        add(firstLabel);
+        add(secondLabel);
+        add(thirdLabel);
+        add(leftButton);
+        add(centerButton);
+        add(rightButton);
+        leftButton.addActionListener(this);
+        centerButton.addActionListener(this);
+        rightButton.addActionListener(this);
+        setForeground(Color.BLACK);
+        setBackground(Color.cyan);
+        x1 = y1 = x2 = y2 = x = y = 0;
+        str = "";
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.drawLine(x1, y1, x2, y2);
+        g.setColor(Color.yellow);
+        g.drawString(str, x, y);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getSource() == leftButton) {
+            str = "您按下了left按钮，标签上的文字的对齐方式将修改为靠左对齐";
+            firstLabel.setAlignment(Label.LEFT);
+            secondLabel.setAlignment(Label.LEFT);
+            thirdLabel.setAlignment(Label.LEFT);
+        }
+        if (e.getSource() == centerButton) {
+            str = "您按下了center按钮，标签上的文字的对齐方式将修改为居中对齐";
+            firstLabel.setAlignment(Label.CENTER);
+            secondLabel.setAlignment(Label.CENTER);
+            thirdLabel.setAlignment(Label.CENTER);
+        }
+        if (e.getSource() == rightButton) {
+            str = "您按下了right按钮，标签上的文字的对齐方式将修改为靠右对齐";
+            firstLabel.setAlignment(Label.RIGHT);
+            secondLabel.setAlignment(Label.RIGHT);
+            thirdLabel.setAlignment(Label.RIGHT);
+        }
+        x2 = 10000;
+        y1 = y2 = 30;
+        y = 55;
+        repaint();
+    }
+
+}
