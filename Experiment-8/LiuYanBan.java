c08920e9240a2380e60e80ac0bd9c3381a479f89
周晨阳 <1448517833@qq.com>
2021-05-22 08:51:56 +0800

    Update LiuYanBan.java

--- Experiment-8/LiuYanBan.java
+++ Experiment-8/LiuYanBan.java
@@ -2,16 +2,28 @@ import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
-//TODO awt's dialog and timer
+/**
+ * 留言板
+ *
+ * @version 3.0
+ */
 public class LiuYanBan extends Applet implements ActionListener, MouseListener {
-    private Label L_head, L_author, L_title, L_content;
+    private static final Frame LiuYanBan = null;
+    private Label L_head, L_author, L_title, L_content, label;
     private TextField T_author, T_title;
     private TextArea A_content;
-    private Button submit, reset, display;
-    private Panel p12, p21, p22, p31, p32, p41, p42, p43;
+    private Button submit, reset, display, button;
+    private Panel p12, p21, p22, p31, p32, p41, p42, p43, p51, panel;
     private String userString;
     private Message message;
+    private Label time;
+    private Timer timer;
+    private String text;
+    private int days, hours, minutes, seconds;
+    private Dialog dialog;
 
     @Override
     public void init() {
@@ -27,9 +39,46 @@ public class LiuYanBan extends Applet implements ActionListener, MouseListener {
 
         A_content = new TextArea(4, 10);
 
-        submit = new Button("login");
-        reset = new Button("guest");
-        display = new Button("display");
+        submit = new Button("登录");
+        reset = new Button("游客登录");
+        display = new Button("注册");
+
+        label = new Label();
+        button = new Button("确定");
+
+        days = hours = minutes = seconds = 0;
+        text = "您已使用本系统" + days + "天" + hours + "小时" + minutes + "分钟" + seconds + "秒";
+        time = new Label(text);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                seconds += 1;
+                if (seconds == 60) {
+                    minutes += 1;
+                    seconds = 0;
+                    if (minutes == 60) {
+                        hours += 1;
+                        minutes = 0;
+                        if (hours == 24) {
+                            days += 1;
+                            hours = 0;
+                        }
+                    }
+                }
+                repaint();
+            }
+        }, 0, 1000);
+
+        dialog = new Dialog(LiuYanBan, "提示", true);
+        dialog.setSize(300, 200);
+        dialog.setLocation(760, 300);
+        panel = new Panel();
+        panel.setFont(new Font("宋体", Font.BOLD, 20));
+        panel.setLayout(new GridLayout(2, 1));
+        panel.add(label);
+        panel.add(button);
+        dialog.add(panel);
+        button.addActionListener(this);
 
         p12 = new Panel();
         p12.setFont(new Font("宋体", Font.BOLD, 28));
@@ -111,6 +160,13 @@ public class LiuYanBan extends Applet implements ActionListener, MouseListener {
         p43.add(new Label(""));
         p43.add(new Label(""));
 
+        p51 = new Panel();
+        p51.setFont(new Font("宋体", Font.BOLD, 20));
+        p51.setLayout(new GridLayout(3, 1));
+        p51.add(new Label(""));
+        p51.add(new Label(""));
+        p51.add(time);
+
         add(new Label(""));
         add(p12);
         add(new Label(""));
@@ -127,9 +183,10 @@ public class LiuYanBan extends Applet implements ActionListener, MouseListener {
         add(p42);
         add(p43);
 
+        add(p51);
+
         L_content.setVisible(false);
         A_content.setVisible(false);
-        display.setVisible(false);
         T_title.setEchoChar('*');
 
         submit.addActionListener(this);
@@ -137,7 +194,6 @@ public class LiuYanBan extends Applet implements ActionListener, MouseListener {
         display.addActionListener(this);
         T_author.addMouseListener(this);
         T_title.addMouseListener(this);
-        A_content.addMouseListener(this);
 
         try {
             message = new Message();
@@ -146,93 +202,143 @@ public class LiuYanBan extends Applet implements ActionListener, MouseListener {
     }
 
     @Override
+    public void paint(Graphics g) {
+        text = "您已使用本系统" + days + "天" + hours + "小时" + minutes + "分钟" + seconds + "秒";
+        time.setText(text);
+    }
+
+    @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == submit) {
-            if (submit.getLabel().equals("login")) {
-                if (T_author.getText().isEmpty()) {
-                    T_author.setText("请输入用户名");
-                } else if (T_title.getText().length() < 6) {
-                    T_title.setEchoChar((char) 0);
-                    T_title.setText("密码错误");
-                } else {
-                    L_content.setVisible(true);
-                    A_content.setVisible(true);
-                    display.setVisible(true);
-                    L_head.setText("留言板");
-                    L_author.setText("作者：");
-                    L_title.setText("标题：");
-                    submit.setLabel("submit");
-                    reset.setLabel("reset");
-                    T_author.setEditable(false);
-                    T_title.setEchoChar((char) 0);
-                    T_title.setText(" ");
-                    T_title.setText("");
-                    userString = T_author.getText();
-                }
-            } else if (submit.getLabel().equals("submit")) {
-                if (T_title.getText().isEmpty() || A_content.getText().isEmpty()) {
-                    reset.setLabel("留言失败");
+            if (submit.getLabel().equals("登录")) {
+                Login login = new Login(T_author.getText(), T_title.getText());
+                if (T_author.getText().isEmpty() || T_title.getText().isEmpty()) {
+                    label.setText("请输入用户名或密码");
+                    dialog.setVisible(true);
+                } else
+                    try {
+                        if (!login.verify()) {
+                            label.setText("用户名或密码错误");
+                            dialog.setVisible(true);
+                        } else {
+                            L_content.setVisible(true);
+                            A_content.setVisible(true);
+                            display.setVisible(true);
+                            L_head.setText("留言板");
+                            L_author.setText("作者：");
+                            L_title.setText("标题：");
+                            submit.setLabel("提交");
+                            reset.setLabel("重置");
+                            display.setLabel("显示");
+                            T_author.setEditable(false);
+                            T_title.setEchoChar((char) 0);
+                            T_title.setText(" ");
+                            T_title.setText("");
+                            userString = T_author.getText();
+                        }
+                    } catch (IOException e1) {
+                        label.setText("验证失败");
+                        dialog.setVisible(true);
+                    }
+            } else if (submit.getLabel().equals("提交")) {
+                if (T_title.getText().isEmpty()) {
+                    label.setText("请输入标题");
+                    dialog.setVisible(true);
+                } else if (A_content.getText().isEmpty()) {
+                    label.setText("请输入内容");
+                    dialog.setVisible(true);
+                } else if (T_title.getText().indexOf("&") != -1 || A_content.getText().indexOf("&") != -1) {
+                    label.setText("不允许输入&符号");
+                    dialog.setVisible(true);
                 } else {
                     MessageSet messageSet = new MessageSet(T_author.getText(), T_title.getText(), A_content.getText());
                     try {
                         messageSet.messageSet();
-                        reset.setLabel("留言成功");
+                        label.setText("留言成功");
+                        dialog.setVisible(true);
                         T_title.setText(" ");
                         A_content.setText(" ");
                         T_title.setText("");
                         A_content.setText("");
                         message = new Message();
-                        display.setLabel("display");
+                        display.setLabel("显示");
                     } catch (IOException e1) {
-                        reset.setLabel("留言失败");
+                        label.setText("留言失败");
+                        dialog.setVisible(true);
                     }
                 }
-            } else if (submit.getLabel().equals("first")) {
+            } else if (submit.getLabel().equals("第一条")) {
                 try {
                     message = new Message();
                     message.message();
                     T_author.setText(message.getAuthor());
                     T_title.setText(message.getTitle());
                     A_content.setText(message.getContent());
-                    reset.setLabel("next");
+                    reset.setLabel("下一条");
                 } catch (IOException e1) {
-                    submit.setLabel("读取失败");
+                    label.setText("读取失败");
+                    dialog.setVisible(true);
+                }
+            } else if (submit.getLabel().equals("注册")){
+                if (T_title.getText().length() < 6) {
+                    label.setText("密码至少6位");
+                    dialog.setVisible(true);
+                } else {
+                    Enroll enroll = new Enroll(T_author.getText(), T_title.getText());
+                    try {
+                        if (enroll.enroll()) {
+                            label.setText("注册成功");
+                            dialog.setVisible(true);
+                            L_head.setText("登录");
+                            submit.setLabel("登录");
+                            display.setLabel("注册");
+                            reset.setVisible(true);
+                        } else {
+                            label.setText("该账户已存在");
+                            dialog.setVisible(true);
+                        }
+                    } catch (IOException e1) {
+                        label.setText("注册失败");
+                        dialog.setVisible(true);
+                    }
                 }
             }
         }
         if (e.getSource() == reset) {
-            if (reset.getLabel().equals("guest")) {
+            if (reset.getLabel().equals("游客登录")) {
                 T_author.setText("游客" + System.currentTimeMillis());
                 L_content.setVisible(true);
                 A_content.setVisible(true);
                 display.setVisible(true);
+                L_head.setText("留言板");
                 L_author.setText("作者：");
                 L_title.setText("标题：");
-                submit.setLabel("submit");
-                reset.setLabel("reset");
+                submit.setLabel("提交");
+                reset.setLabel("重置");
+                display.setLabel("显示");
                 T_author.setEditable(false);
                 T_title.setEchoChar((char) 0);
                 T_title.setText(" ");
                 T_title.setText("");
                 userString = T_author.getText();
-            } else if (reset.getLabel().equals("reset")) {
+            } else if (reset.getLabel().equals("重置")) {
                 T_title.setText(" ");
                 A_content.setText(" ");
                 T_title.setText("");
                 A_content.setText("");
-            } else if (reset.getLabel().equals("next")) {
+            } else if (reset.getLabel().equals("下一条")) {
                 try {
                     message.message();
                     T_author.setText(message.getAuthor());
                     T_title.setText(message.getTitle());
                     A_content.setText(message.getContent());
                 } catch (Exception e1) {
-                    reset.setLabel("at last");
+                    reset.setLabel("最后一个");
                 }
             }
         }
         if (e.getSource() == display) {
-            if (display.getLabel().equals("display")) {
+            if (display.getLabel().equals("显示")) {
                 try {
                     message.message();
                     T_author.setText(message.getAuthor());
@@ -240,54 +346,69 @@ public class LiuYanBan extends Applet implements ActionListener, MouseListener {
                     A_content.setText(message.getContent());
                     T_title.setEditable(false);
                     A_content.setEditable(false);
-                    display.setLabel("close");
-                    submit.setLabel("first");
-                    reset.setLabel("next");
+                    display.setLabel("返回");
+                    submit.setLabel("第一条");
+                    reset.setLabel("下一条");
                 } catch (IOException e1) {
-                    display.setLabel("读取失败");
+                    label.setText("读取失败");
+                    dialog.setVisible(true);
                 } catch (Exception exception) {
                     try {
                         message = new Message();
                     } catch (IOException e1) {
-                        display.setLabel("无留言");
+                        label.setText("无留言");
+                        dialog.setVisible(true);
                     }
                 }
-            } else if (display.getLabel().equals("close")) {
-                T_author.setText(userString);
+            } else if (display.getLabel().equals("返回")) {
+                if (L_head.getText().equals("留言板")) {
+                    T_author.setText(userString);
+                    T_title.setText(" ");
+                    A_content.setText(" ");
+                    T_title.setText("");
+                    A_content.setText("");
+                    T_title.setEditable(true);
+                    A_content.setEditable(true);
+                    submit.setLabel("提交");
+                    reset.setLabel("重置");
+                    display.setLabel("显示");
+                } else if (L_head.getText().equals("注册")) {
+                    L_head.setText("登录");
+                    submit.setLabel("登录");
+                    reset.setVisible(true);
+                }
+            } else if (display.getLabel().equals("注册")) {
+                L_head.setText("注册");
+                submit.setLabel("注册");
+                display.setLabel("返回");
+                T_author.setText(" ");
                 T_title.setText(" ");
-                A_content.setText(" ");
+                T_author.setText("");
                 T_title.setText("");
-                A_content.setText("");
-                T_title.setEditable(true);
-                A_content.setEditable(true);
-                submit.setLabel("submit");
-                reset.setLabel("reset");
-                display.setLabel("display");
+                reset.setVisible(false);
             }
         }
+        if (e.getSource() == button) {
+            dialog.setVisible(false);
+        }
+
         repaint();
+
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         if (e.getSource() == T_author) {
-            if (L_author.getText().equals("用户名：")){
+            if (L_author.getText().equals("用户名：")) {
                 T_author.setText(" ");
                 T_author.setText("");
             }
         }
         if (e.getSource() == T_title) {
-            if (L_title.getText().equals("密码：")){
+            if (L_title.getText().equals("密码：")) {
                 T_title.setText(" ");
                 T_title.setText("");
-                T_title.setEchoChar('*');
             }
-            if (L_title.getText().equals("标题：")){
-                reset.setLabel("reset");
-            }
-        }
-        if (e.getSource() == A_content) {
-            reset.setLabel("reset");
         }
     }
 
