987e6583611d24d34642c0a9c04a7c02ad22ba1b
周晨阳 <1448517833@qq.com>
2021-05-24 18:46:59 +0800

    Update HelloEducation.java

--- Experiment-1/HelloEducation.java
+++ Experiment-1/HelloEducation.java
@@ -1,7 +1,7 @@
 
 //这是我们的第一个java.applet，该程序保存在文件HelloEducation.java中
-import java.awt.Graphics;//在进行显示输出时，需要用到类Graphics的对象；
 import java.applet.Applet;//Applet类是所有的java.applet的父类;
+import java.awt.Graphics;//在进行显示输出时，需要用到类Graphics的对象；
 
 public class HelloEducation extends Applet {
     // 程序员给这个类取名为HelloEducation
